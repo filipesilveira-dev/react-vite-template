63811090c885606254d66aa3f7171ebee216aa49
Filipe <filipesilveira.dev@gmail.com>
2026-06-17 13:04:36 -0300

    feat: template base limpo com vite, ts e tailwind

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+
+function App() {
+  return (
+    <div className="flex h-screen items-center justify-center bg-slate-900">
+      <h1 className="text-4xl font-bold text-emerald-400 drop-shadow-md">
+        Template Prontinho! 🚀
+      </h1>
+    </div>
+  );
+}
+
+export default App;
